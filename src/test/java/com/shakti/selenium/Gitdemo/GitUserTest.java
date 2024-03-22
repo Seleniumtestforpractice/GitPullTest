8973cd9247e4135c6bc349de9c05732eeff24766
Shakti Samal <shaktisamalit@gmail.com>
2024-03-22 18:01:27 +0530

    commit from IN user

--- src/test/java/com/shakti/selenium/Gitdemo/GitUserTest.java
+++ src/test/java/com/shakti/selenium/Gitdemo/GitUserTest.java
@@ -2,6 +2,7 @@ package com.shakti.selenium.Gitdemo;
 
 public class GitUserTest {
 	public static void main(String[] args) {
+		System.out.println("GIT IN USER TEST");
 		System.out.println("GIT US USER TEST");
 	}
 }
