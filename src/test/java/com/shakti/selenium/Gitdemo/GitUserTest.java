c8463a45bcd1001a35798c327cb7cba58380f6d8
Shakti Samal <shaktisamalit@gmail.com>
2024-03-24 09:18:54 +0530

    develop branch commit

--- src/test/java/com/shakti/selenium/Gitdemo/GitUserTest.java
+++ src/test/java/com/shakti/selenium/Gitdemo/GitUserTest.java
@@ -4,5 +4,6 @@ public class GitUserTest {
 	public static void main(String[] args) {
 		System.out.println("GIT IN USER TEST");
 		System.out.println("GIT US USER TEST");
+		System.out.println("GIT DEVELOP BRANCH CREATED");
 	}
 }
